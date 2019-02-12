68281a60db319bbd83ab6554681654e6c317a005
Ghabe Bossin <gee.bossin@gmail.com>
2019-02-12 07:27:53 +0000

    commiting

--- src/index.jsx
+++ src/index.jsx
@@ -9,3 +9,16 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('react-root'));
+
+// ok, I wasn't sure, I've never used one before. It's giving me an error?
+
+//oh hang on a sec that's JSHint
+// it's supposed to be ESLint
+//get rid of your JSHintrc
+
+// check your file directory
+// .jshintrc in this project directory
+//in vagrant? in this project?kk
+
+// if you have the jshint extension installed you should disable it as well
+

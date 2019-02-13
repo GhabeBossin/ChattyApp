4eb0ca54cf8ca4c46565fd95872f391251b08de8
Ghabe Bossin <gee.bossin@gmail.com>
2019-02-13 03:58:23 +0000

    handles name and content submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,83 @@
+/* eslint-disable no-console */
 import React, { Component } from 'react';
 import MessageList from './MessageList.jsx';
 import Chatbar from './Chatbar.jsx';
-// import {randomstring} from 'randomstring';
+import randomstring from 'randomstring'
 
 
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       currentUser: {name: 'Bob'}, // optional. if currentUser is not defined, it means the user is Anonymous
       messages: [
         {
-          id: 1, //randomstring(),
+          id: 1,
           username: 'Bob',
           content: 'Has anyone seen my marbles?',
         },
         {
-          id: 2, //randomstring(),
+          id: 2,
           username: 'Anonymous',
           content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
         }
       ]
     }
+    this.handleMessageSub = this.handleMessageSub.bind(this)
+    this.handleNameSub = this.handleNameSub.bind(this)
   }
 
   componentDidMount() {
-    console.log('componentDidMount <App />');
-    setTimeout(() => {
-      console.log('Simulating incoming message');
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+    // console.log('componentDidMount <App />');
+    // setTimeout(() => {
+    //   console.log('Simulating incoming message');
+    //   // Add a new message to the list of messages in the data store
+    //   const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
+    //   const messages = this.state.messages.concat(newMessage)
+
+    //   this.setState({messages: messages})
+    // }, 3000);
+
+    // handleNameSub(event) {
+    //   // if (event.key !== 'Enter') {
+    //   //   return
+    //   // }
+    //   // console.log(event.key)
+    //   const newCurrentUser = {name: event.target.value}
+    //   console.log(event.target.value)
+    //   this.setState({currentUser: newCurrentUser})
+    // }
+
+    // handleMessageSub(event) {
+    //   if (event.key !== 'Enter') {
+    //     return
+    //   }
+    //   console.log(event.target.value);
+    //   const newMessage = {username: this.state.currentUser.name, content: event.target.value, id: randomstring.generate(9) }
+    //   const messages = this.state.messages.concat(newMessage)
+    //   this.setState({messages: messages});
+    //   input.value = '';
+    // }
+  }
+
+  handleNameSub(event) {
+    console.log(event.target);
+    const newCurrentUser = {name: event.target.value}
+    // console.log(event.target.value)
+    this.setState({currentUser: newCurrentUser})
+
+  }
+
+  handleMessageSub(event) {
+    if (event.key !== 'Enter') {
+      return
+    }
+    // console.log(event.target.value);
+    const newMessage = {username: this.state.currentUser.name, content: event.target.value, id: randomstring.generate(9) }
+    const messages = this.state.messages.concat(newMessage)
+    this.setState({messages: messages});
+    // input.value = '';
   }
 
   render() {
@@ -44,7 +87,7 @@ class App extends Component {
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
         <MessageList messages={this.state.messages}/>
-        <Chatbar currentUser={this.state.currentUser}/>
+        <Chatbar currentUser={this.state.currentUser} handleNameSub={this.handleNameSub} handleMessageSub={this.handleMessageSub}/>
       </div>
     );
   }
--- src/Chatbar.jsx
+++ src/Chatbar.jsx
@@ -3,15 +3,12 @@ import React, {Component} from 'react';
 // {this.props.currentUser}
 
 class Chatbar extends Component {
-  // constructor(currentUser) {
-  //   super({currentUser});
-
-  // }
   render() {
     return (
-      <footer className="chatbar">
-        <input defaultValue={this.props.currentUser.name} className="chatbar-username" placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+      <footer className='chatbar'>
+        <input value={this.props.value} onBlur={this.props.handleNameSub} defaultValue={this.props.currentUser.name} className='chatbarUsername' placeholder='Your Name (Optional)' />
+
+        <input value={this.props.value} onKeyUp={this.props.handleMessageSub} type='text' className='chatbarMessage' placeholder='Type a message and hit ENTER' />
       </footer>
     )
   }

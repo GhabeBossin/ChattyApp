46b1b922c86e1aa2c8dd3ff97feefdf689bcae26
Ghabe Bossin <gee.bossin@gmail.com>
2019-02-13 21:02:45 +0000

    redirecting repo

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // WS server
 const express = require('express');
 const SocketServer = require('ws').Server;
@@ -7,7 +8,7 @@ const PORT = 3001;
 
 // Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
+  // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
